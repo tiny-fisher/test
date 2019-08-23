a1a86e247718c7b727f879ef58b6485f2f3874fc
tiny-fisher <824400492@qq.com>
2019-08-23 20:28:54 +0800

    forget commit

--- src/main/java/com/nowcoder/mavendemo1/App.java
+++ src/main/java/com/nowcoder/mavendemo1/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
+        // change for test
+        // change for another time
         System.out.println( "Hello World!" );
     }
 }
